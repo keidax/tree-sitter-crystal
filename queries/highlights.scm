775ef2c9c006d250842d398ff1346b11d628bf93
Hugo Parente Lima <hugo.pl@gmail.com>
2024-12-06 17:15:12 -0300

    Improve highlighting.

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -11,6 +11,7 @@
   "class"
   "def"
   "do"
+  "of"
   "end"
   "ensure"
   "in"
@@ -52,11 +53,8 @@
 
 ; Operators
 
-[
-"="
-"=>"
-"->"
-] @operator
+(operator) @operator
+"=" @operator
 
 [
   ","
@@ -73,17 +71,31 @@
   "}"
 ] @punctuation.bracket
 
-(class_def name: [(constant) (generic_type)] @type)
+(class_def name: [(constant) (generic_type)] @type.definition)
 
 (module_def name: [(constant) (generic_type)] @module)
 
-(struct_def name: [(constant) (generic_type)] @type)
+(struct_def name: [(constant) (generic_type)] @type.definition)
 
 (enum_def name: [(constant)] @type)
 
 (method_def name: [(identifier) (constant)] @function.method)
 
+(method_def name: (identifier) @constructor (#eq? @constructor "initialize"))
+
 (param name: [(identifier)] @variable.parameter)
+
+(param type: [(constant) (generic_instance_type)] @type)
+
+(call receiver: [(constant) (generic_type)] @type)
+
+(array of: (constant) @type)
+(hash [(constant)] @type)
+(generic_instance_type) @type
+
+(call method: (identifier) @keyword (#eq? @keyword "new"))
+
+(call method: (identifier) @function.call (#not-eq? @function.call "new"))
 
 [
   (class_var)
